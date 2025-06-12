72d708e479918ef61f29690679ed4ffb8efedf55
HamzaFerwana <hamza_ferwana@hotmail.com>
2025-06-12 14:08:22 +0300

    Test Commit4

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,13 @@
 
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\File;
 
 
 Auth::routes();
 
+Route::get('/debug-files', function () {
+    $files = File::files(public_path('uploads'));
+    return response()->json($files);
+});
 
